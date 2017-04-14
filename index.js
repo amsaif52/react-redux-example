8959b44cd455ad598fbc0eba6e543e5621cb7389
amsaif52 <am52saif@gmail.com>
2017-04-14 18:22:21 -0400

    Initial Commit....

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './views/counter/counter';
+
+
+ReactDOM.render(<Counter/>,document.getElementById('app'));
